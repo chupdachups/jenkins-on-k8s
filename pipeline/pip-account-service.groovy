d2df24e6e06cb16331afcc7ef9c7c105757f9c00
chupdachups <chupdachups@gmail.com>
2022-05-02 02:41:40 +0900

    commit

--- pipeline/pip-account-service.groovy
+++ pipeline/pip-account-service.groovy
@@ -8,10 +8,10 @@ apiVersion: v1
 kind: Pod
 metadata:
 labels:
-  component: ci
+  component: cicd
 spec:
   # Use service account that can deploy to all namespaces
-  serviceAccountName: jenkins-agent
+  serviceAccountName: jenkins-test-admin
   containers:
   - name: git
     image: alpine/git:v2.34.2
@@ -34,6 +34,11 @@ spec:
     volumeMounts:
     - mountPath: /var/run/docker.sock
       name: docker-sock
+  - name: curl
+    image: alpine/curl:3.14
+    command:
+    - cat
+    tty: true
   volumes:
     - name: docker-sock
       hostPath:
@@ -41,6 +46,9 @@ spec:
     - name: m2
       persistentVolumeClaim:
         claimName: m2
+    - name: kubeconfig
+      hostPath:
+        path: /home/azureuser/.kube/config
 """
 }
    }
@@ -68,6 +76,7 @@ spec:
         container('docker') {
           sh """
              docker build -t chupdachups/account-service:$BUILD_NUMBER -f account-service/dockerfile account-service
+             docker tag chupdachups/account-service:$BUILD_NUMBER chupdachups/account-service:latest
           """
         }
       }
@@ -86,9 +95,199 @@ spec:
         container('docker') {
           sh """
              docker push chupdachups/account-service:$BUILD_NUMBER
+             docker push chupdachups/account-service:latest
           """
         }
       }
     }
+	stage('deploy-deploy') {
+	  steps {
+	    container('curl') {
+		  sh """
+		    #내부 API 서버 호스트 이름을 가리킨다
+			APISERVER=https://kubernetes.default.svc
+			echo \${APISERVER}
+
+			#서비스어카운트(ServiceAccount) 토큰 경로
+			SERVICEACCOUNT=/var/run/secrets/kubernetes.io/serviceaccount
+
+			#이 파드의 네임스페이스를 읽는다
+			NAMESPACE=\$(cat \${SERVICEACCOUNT}/namespace)
+
+			#서비스어카운트 베어러 토큰을 읽는다
+			TOKEN=\$(cat \${SERVICEACCOUNT}/token)
+
+			#내부 인증 기관(CA)을 참조한다
+			CACERT=\${SERVICEACCOUNT}/ca.crt
+			
+			# TOKEN으로 API를 탐색한다
+			curl --cacert \${CACERT} \\
+			--header "Authorization: Bearer \${TOKEN}" \\
+			-X POST \${APISERVER}/apis/apps/v1/namespaces/\${NAMESPACE}/deployments \\
+			--header 'Content-Type: application/json' \\
+			--data-raw '{
+  "apiVersion": "apps/v1",
+  "kind": "Deployment",
+  "metadata": {
+    "name": "account-service",
+    "namespace": "jenkins-workspace",
+    "labels": {
+      "app": "account-service"
+    }
+  },
+  "spec": {
+    "replicas": 1,
+    "selector": {
+      "matchLabels": {
+        "app": "account-service"
+      }
+    },
+    "template": {
+      "metadata": {
+        "labels": {
+          "app": "account-service"
+        }
+      },
+      "spec": {
+        "containers": [
+          {
+            "name": "account-service",
+            "image": "chupdachups/account-service:latest",
+            "ports": [
+              {
+                "containerPort": 8070
+              }
+            ]
+          }
+        ]
+      }
+    }
+  }
+}'
+		  """
+		}
+	  }
+	}
+	stage('deploy-patch') {
+	  steps {
+	    container('curl') {
+		  sh """
+			APISERVER=https://kubernetes.default.svc
+			SERVICEACCOUNT=/var/run/secrets/kubernetes.io/serviceaccount
+			NAMESPACE=\$(cat \${SERVICEACCOUNT}/namespace)
+			TOKEN=\$(cat \${SERVICEACCOUNT}/token)
+			CACERT=\${SERVICEACCOUNT}/ca.crt
+			curl --cacert \${CACERT} \\
+			--header "Authorization: Bearer \${TOKEN}" \\
+			-X PATCH \${APISERVER}/apis/apps/v1/namespaces/\${NAMESPACE}/deployments \\
+			--header 'application/strategic-merge-patch+json' \\
+			--data-raw '{
+  "apiVersion": "apps/v1",
+  "kind": "Deployment",
+  "metadata": {
+    "name": "account-service",
+    "namespace": "jenkins-workspace",
+    "labels": {
+      "app": "account-service"
+    }
+  },
+  "spec": {
+    "replicas": 1,
+    "selector": {
+      "matchLabels": {
+        "app": "account-service"
+      }
+    },
+    "template": {
+      "metadata": {
+        "labels": {
+          "app": "account-service"
+        }
+      },
+      "spec": {
+        "containers": [
+          {
+            "name": "account-service",
+            "image": "chupdachups/account-service:latest",
+            "ports": [
+              {
+                "containerPort": 8070
+              }
+            ]
+          }
+        ]
+      }
+    }
+  }
+}'
+		  """
+		}
+	  }
+	}
+	stage('deploy-service') {
+	  steps {
+	    container('curl') {
+		  sh """
+			APISERVER=https://kubernetes.default.svc
+			SERVICEACCOUNT=/var/run/secrets/kubernetes.io/serviceaccount
+			NAMESPACE=\$(cat \${SERVICEACCOUNT}/namespace)
+			TOKEN=\$(cat \${SERVICEACCOUNT}/token)
+			CACERT=\${SERVICEACCOUNT}/ca.crt
+			curl --cacert \${CACERT} \\
+			--header "Authorization: Bearer \${TOKEN}" \\
+			-X POST \${APISERVER}/api/v1/namespaces/\${NAMESPACE}/services \\
+			--header 'Content-Type: application/json' \\
+			--data-raw '{
+  "apiVersion": "v1",
+  "kind": "Service",
+  "metadata": {
+    "name": "account-service",
+    "namespace": "jenkins-workspace"
+  },
+  "spec": {
+    "type": "ClusterIP",
+    "ports": [
+      {
+        "port": 8070,
+        "targetPort": 8070
+      }
+    ],
+    "selector": {
+      "app": "account-service"
+    }
+  }
+}'
+		  """
+		}
+	  }
+	}
+    stage('rollout') {
+	  steps {
+	    container('curl') {
+		  sh """
+			APISERVER=https://kubernetes.default.svc
+			SERVICEACCOUNT=/var/run/secrets/kubernetes.io/serviceaccount
+			NAMESPACE=\$(cat \${SERVICEACCOUNT}/namespace)
+			TOKEN=\$(cat \${SERVICEACCOUNT}/token)
+			CACERT=\${SERVICEACCOUNT}/ca.crt
+			curl --cacert \${CACERT} \\
+			--header "Authorization: Bearer \${TOKEN}" \\
+			-X PATCH \${APISERVER}/apis/apps/v1/namespaces/jenkins-workspace/deployments/account-service \\
+			--header 'Content-Type: application/strategic-merge-patch+json' \\
+			--data-raw '{
+    "spec": {
+        "template": {
+            "metadata": {
+                "annotations": {
+                    "kubectl.kubernetes.io/restartedAt": "<time.Now()>"
+                }
+            }
+        }
+    }
+}'
+		  """
+		}
+      }
+	}
   }
 }
